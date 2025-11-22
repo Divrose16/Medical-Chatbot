3428378b53f415ae5574cab2a7869c8207a43786
Divrose <123873257+Divrose16@users.noreply.github.com>
2025-11-22 13:33:21 +0530

    Update prompt.py

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces o retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "\n\n"
+    "{context}"
+)
